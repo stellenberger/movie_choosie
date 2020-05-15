07a5dc78bf861a6498fd593e257ef227652aec94
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-05-15 10:29:12 +0100

    Appended all search results onto the page

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 fetch("http://www.omdbapi.com/?apikey=" + config.apiKey + "&s=friends").then(function(response) {
   if(response.ok) {
     return response.json();
@@ -5,5 +7,13 @@ fetch("http://www.omdbapi.com/?apikey=" + config.apiKey + "&s=friends").then(fun
 		return Promise.reject(response);
 	}
 }).then(function(data) {
-  console.log(data)
+  let searchResults = data.Search
+  for(let i = 0; i < searchResults.length; i++) {
+    let div = document.createElement("div");
+    let result = document.createTextNode(searchResults[i].Title);
+    div.appendChild(result);
+    document.getElementById('app').appendChild(result);
+  }
+}).catch(function(error) {
+  console.warn("Something went wrong", error);
 })
