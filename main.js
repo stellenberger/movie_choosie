041db34bd9755e1f5502a08829ae0a74fd296f82
Stephan Ellenberger <s.ellenberger95@gmail.com>
2020-05-15 11:21:22 +0100

    Search results were not displaying in divs but now are

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 
-
-fetch("http://www.omdbapi.com/?apikey=" + config.apiKey + "&s=friends").then(function(response) {
+// Stores the API key in a hidden file and gets it out
+let apiKey = config.apiKey
+// Will eventually store the search query from the user
+let searchQuery = "friends"
+// Fetches the api data from omdb and adds the api key and search query to the query string
+// then will check the response of the data
+fetch("http://www.omdbapi.com/?apikey=" + config.apiKey + "&s=" + searchQuery).then(function(response) {
   if(response.ok) {
     return response.json();
   } else {
 		return Promise.reject(response);
-	}
+  }
+  // if all goes well, the data will then be used
 }).then(function(data) {
+  // Store the specific search data in a variable called searchResults
   let searchResults = data.Search
+  // iterate through the results 
   for(let i = 0; i < searchResults.length; i++) {
+    // create a new div for each result
     let div = document.createElement("div");
+    // add the result into a text node and store it in a variable
     let result = document.createTextNode(searchResults[i].Title);
+    // add that text node to the div element you created
     div.appendChild(result);
-    document.getElementById('app').appendChild(result);
+    // find the main div with id app and store it in a variable
+    let app = document.getElementById('app')
+    // append the div with the search result into the main app element
+    app.appendChild(div);
   }
 }).catch(function(error) {
   console.warn("Something went wrong", error);
